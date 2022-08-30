c614453d3d76295a6e92b7c0d18afa99d314d20b
Maurilo Santos <109925535+maurilosantos@users.noreply.github.com>
2022-08-30 18:06:01 -0300

    commit script ok

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,68 +1,34 @@
+var inputAdultos = document.getElementById("inputAdultos");
+var inputCriancas = document.getElementById("inputCriancas");
+var inputDuracao = document.getElementById("inputDuracao");
+var ExpCarne = 0;
+var ExpCerveja = 0;
+var ExpBebida = 0;
 
+function calcular() {
+    let adultos = inputAdultos.value
+    let criancas = inputCriancas.value
+    let duracao = inputDuracao.value
 
-let inputHomens = document.getElementById("homens");
-let inputMulheres = document.getElementById("mulheres");
-let inputCriancas = document.getElementById("criancas");
-let inputDuracao = document.getElementById("duracao");
+    ExpCarne = (((adultos * qtdCarne(duracao)) + (criancas * qtdCarne(duracao) / 2)) / 1000).toFixed(1)
+    ExpCerveja = Math.ceil(adultos * qtdCerveja(duracao))
+    ExpBebida = Math.ceil((adultos * qtdBebida(duracao)) + (criancas * qtdBebida(duracao) / 2))
 
-let resultado = document.getElementById("resultado");
-
-function calcular(){
-    console.log("Calculando...");
-    let homens = inputHomens.value;
-    let mulheres = inputMulheres.value;
-    let criancas = inputCriancas.value;
-    let duracao = inputDuracao.value;
-
-    let qtdTotalCarne = carnePH(duracao) * homens + carnePM(duracao) * mulheres + (carnePH(duracao)/2 * criancas);
-    let qtdTotalCerveja = cervejaPP(duracao) * (homens + mulheres);
-    let qtdTotalBebidas = bebidasPP(duracao) * (homens + mulheres) + (bebidasPP(duracao)/2 * criancas);
-    let qtdTotalAcompanhamento = AcompanhamPP(duracao) * (homens + mulheres) + (AcompanhamPP(duracao)/2 * criancas);
-
-    resultado.innerHTML = `<p><img src="./assets/meat.png" width="35px"> ${qtdTotalCarne/1000} Kg de Carne</p>`
-    resultado.innerHTML += `<p><img src="./assets/cheers.png" width="35px"> ${Math.ceil(qtdTotalCerveja/350)} Latas de Cerveja (350ml)</p>`
-    resultado.innerHTML += `<p><img src="./assets/soda.png" width="35px"> ${Math.ceil(qtdTotalBebidas/2000)} Garrafas (2L) de bebida não-alcoólica</p>`
-    resultado.innerHTML += `<p><img src="./assets/espeto.png" width="35px"> ${qtdTotalAcompanhamento/1000} Kg de Acompanhamentos</p>`
-function carnePH(duracao){
     
-    if (duracao >= 6){
-        return 650;
-    } else {
-        return 400;
-    }
-    }
-    
-    function carnePM(duracao){
+    document.getElementById("carne").innerHTML = ExpCarne + " Kg de Carne";
+    document.getElementById("cerveja").innerHTML = ExpCerveja + " Latinhas de Cerveja (350ml)";
+    document.getElementById("bebidas").innerHTML = ExpBebida + " Garrafas não-alcoólicas (1L)";
+    duracao > 0 ? document.getElementById("resultado").style.display = "inline" : document.getElementById("resultado").style.display = "none"
+}
 
-     if (duracao >= 6){
-            return 550;
-    } else{
-        return 300;
-    }
-    }
-    
-    function cervejaPP(duracao){
-        
-        if (duracao >= 6){
-            return 2000;
-        } else {
-            return 1200;
-        }
-        }
+function qtdCarne(duracao) {
+     return duracao >= 6 ? 650 : 400
+}
 
-        function bebidasPP(duracao){
-            
-            if (duracao >= 6){
-                return 1500;
-            } else {
-                return 1000;
-            }
-            }
-        function AcompanhamPP(duracao){
-            if (duracao >= 6){
-                return 450;
-            } else {
-                return 200;
-            }
-        }
-  }
+function qtdCerveja(duracao) {
+    return duracao >= 6 ? 6 : 4
+}
+
+function qtdBebida(duracao) {
+    return duracao > 6 ? 1.5 : 1
+}
